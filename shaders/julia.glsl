139dd1f227e3c22206fc26ffaf65834af91a6382
Александр Фролов <froller@froller.net>
2020-06-03 04:35:39 +0300

    Комплексная математика

--- shaders/julia.glsl
+++ shaders/julia.glsl
@@ -1,27 +1,31 @@
 #version 330 core
 
+// Complex math
+#define cplxmul(a, b) vec2(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x)
+#define cplxcon(a)    vec2(a.x,-a.y)
+#define cplxdiv(a, b) vec2(((a.x*b.x+a.y*b.y)/(b.x*b.x+b.y*b.y)),((a.y*b.x-a.x*b.y)/(b.x*b.x+b.y*b.y)))
+
 in vec2 UV;
 
-//const vec2 C = vec2(0.1, 0.63);
 uniform vec2 C;
 uniform float sensitivity;
 
-vec2 juliaTransform(const vec2 Z);
+vec2 transform(const vec2 Z);
 vec3 getColor(const vec2 point);
 
 void main() {
     gl_FragColor = vec4(getColor(UV * 2), 1.0);
 }
 
-vec2 juliaTransform(const vec2 Z) {
-    return vec2(Z.x * Z.x - Z.y * Z.y, Z.x * Z.y + Z.y * Z.x) + C * 2;
+vec2 transform(const vec2 Z) {
+    return cplxmul(Z, Z) + C * 2;
 }
 
 vec3 getColor(const vec2 point) {
     float c = 0;
     vec2 z = point + vec2(0, 0);
     while (c < 1.0 && length(z) < 1000.0) {
-        z = juliaTransform(z);
+        z = transform(z);
         c += sensitivity;
     }
     return vec3(c);
