05c437923c166dd8299acb8738075cdda6116acb
Александр Фролов <froller@froller.net>
2020-06-03 16:26:21 +0300

    Цвета и более вменяемые ограничения

--- shaders/julia.glsl
+++ shaders/julia.glsl
@@ -5,6 +5,9 @@
 #define cplxcon(a)    vec2(a.x,-a.y)
 #define cplxdiv(a, b) vec2(((a.x*b.x+a.y*b.y)/(b.x*b.x+b.y*b.y)),((a.y*b.x-a.x*b.y)/(b.x*b.x+b.y*b.y)))
 
+// Rainbow generator
+#define rainbow(x, o) float(2 * (abs(x - o) - abs(x - 0.2 - o) - abs(x - 0.4 - o) + abs(x - 0.6 - o)))
+
 in vec2 UV;
 
 uniform vec2 C;
@@ -24,9 +27,9 @@ vec2 transform(const vec2 Z) {
 vec3 getColor(const vec2 point) {
     float c = 0;
     vec2 z = point + vec2(0, 0);
-    while (c < 1.0 && length(z) < 1000.0) {
+    while (c <= 1.0 && length(z) < 2.0) {
         z = transform(z);
         c += sensitivity;
     }
-    return vec3(c);
+    return vec3(rainbow(c, 0.0), rainbow(c, 0.2), rainbow(c, 0.4));
 }
